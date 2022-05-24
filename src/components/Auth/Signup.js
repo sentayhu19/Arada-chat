5402af60c1df6d7768e48799492417064cab0820
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2022-05-24 17:11:28 +0300

    Signup page css added

--- src/components/Auth/Signup.js
+++ src/components/Auth/Signup.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react'
-
+import './Signup.css';
 export default class Signup extends Component {
   render() {
     return (
-      <div>signup</div>
+      <section className='main-wrap'>
+        <h2 className='logo'>Arada Chat</h2>
+      <form className='signup-wrap'>
+        <input type="text" required placeholder='User Name'/>
+        <input type="email" required placeholder='Email Address'/>
+        <input type="password" required placeholder='Password'/>
+        <input type="password" required placeholder='Password Confrimation'/>
+        <button type='submit' className='submit'>Submit</button>
+    </form>
+    <div className='login'>
+      <p>Already a user? Login</p>
+    </div>
+      </section>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Auth/Login';
 import Signup from './components/Auth/Signup';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
