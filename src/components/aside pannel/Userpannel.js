94f49cb49b78807573c94beb35e0a906003d006b
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2022-05-25 21:53:47 +0300

    Select option added

--- src/components/aside pannel/Userpannel.js
+++ src/components/aside pannel/Userpannel.js
@@ -4,6 +4,11 @@ import React from 'react'
   return (
     <header>
         <h1 className='logo'>አrada Chat</h1>
+        <select className='menu-options'>
+            <option value="user">Signed as User</option>
+            <option value="avatar">Change Avatar</option>
+            <option value="signout">Sign Out</option>
+        </select>
     </header>
   )
 }
