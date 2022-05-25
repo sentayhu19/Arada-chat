b8ee0c08325bcd1dc70816b105701b43f3c4aaca
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2022-05-26 00:05:29 +0300

    More styling on Modal

--- src/components/aside pannel/Channel.js
+++ src/components/aside pannel/Channel.js
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowsLeftRight } from '@fortawesome/free-solid-svg-icons'
 import { faPlus } from '@fortawesome/free-solid-svg-icons'
 export default function Channel() {
+    const [modal, setModal] = useState({ModalShow: false});
+    const handleClick = () => { 
+    setModal({ModalShow: true})
+    console.log("CLicked")
+    }
+    const removeModal = () =>{
+        setModal({ModalShow: false});
+    }
   return (
     <div>
         <div className='channels-wrap'>
@@ -11,8 +19,27 @@ export default function Channel() {
             CHANNELS (0)
             
             </h4>
-            <FontAwesomeIcon className='channels' icon={faPlus}/>
+            <FontAwesomeIcon onClick={handleClick} className='channels' icon={faPlus}/>
         </div>
+        {modal.ModalShow ?
+             <div className='modal'>
+                 <div className='modal-sub'>
+             <h2 className='create-new'>Create New Chaneel</h2>
+             <form>
+             <div className='channel-input'>
+             <input type="text" required className='channel-name' placeholder='Channel Name'/>
+             <textarea className='channel-description' rows="4" cols="60" maxLength="200" placeholder='Channel Description in 200 characters'/>
+             </div>
+             <div className='create-channel-btn-wrap'>
+             <button type='submit' className='create'>Create New Channel</button>
+             <button type='submit' className='cancel' onClick={removeModal}>Cancel</button>
+             </div>
+             </form>
+             </div>
+             </div>
+         :
+        
+        ''}
         </div>
   )
 }
