84dd19dc0aab90e181bc6c7af700b5afa82a1886
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2022-05-26 11:54:40 +0300

    Display list of Channels on aside pannel

--- src/components/aside pannel/Channel.js
+++ src/components/aside pannel/Channel.js
@@ -1,11 +1,16 @@
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowsLeftRight } from '@fortawesome/free-solid-svg-icons'
 import { faPlus } from '@fortawesome/free-solid-svg-icons'
 import firebase from '../../firebase';
 import { useSelector } from 'react-redux/es/exports';
+import Channeld from './Channeld';
 
 export default function Channel() {
+    useEffect(()=>{
+channelLoader();
+
+    },[])
     const [modal, setModal] = useState({ModalShow: false});
     const { currentUser, loading } = useSelector((state)=> state.userReducer);
     const [channel, setChannel] = useState({
@@ -15,6 +20,17 @@ export default function Channel() {
         channelDetails: "",
         channelRef: firebase.database().ref('channels'),
     })
+    const channelLoader = () => {
+        const loadedchannelsList=[];
+        channel.channelRef.on('child_added',collect =>{
+      loadedchannelsList.push(collect.val());
+      const ch ="channel";
+      setChannel((e) => ({ ...e,
+        [ch]: loadedchannelsList}));
+      console.log("load on state: ",channel);
+        })
+    }
+    
     const handleClick = () => { 
     setModal({ModalShow: true})
     }
@@ -61,11 +77,17 @@ avatar: currentUser.photoURL,
         <div className='channels-wrap'>
         <h4 className='channels'>
             <FontAwesomeIcon icon={faArrowsLeftRight}/>
-            CHANNELS (0)
+            CHANNELS ({channel.channel.length})
             
             </h4>
             <FontAwesomeIcon onClick={handleClick} className='channels' icon={faPlus}/>
         </div>
+        <ul className='channels-list'>
+        {channel.channel.map((c)=> (             
+               <Channeld channelData={c.name} />
+            )) 
+                }
+                </ul>
         {modal.ModalShow ?
              <div className='modal'>
                  <div className='modal-sub'>
---
+++ src/components/aside pannel/Channeld.js
@@ -0,0 +1,8 @@
+const Channeld = ({channelData}) => {
+    return (
+    <li>
+        <p># {channelData}</p>
+    </li>
+    )
+}
+export default Channeld;
