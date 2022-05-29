a2c56ca47cf88bbdcc277aedc7bde6459d3c3c37
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2022-05-29 14:11:23 +0300

    Functionality to send and display sent messages

---
+++ src/components/Messages/Message.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { mockComponent } from 'react-dom/test-utils';
+import "./Message.css";
+import moment from 'moment';
+
+const Message = ({message}) => {
+   const {conetent, timestamp,user, useravatar,  username } = message;
+   const timeFromNow = timestamp => moment(timestamp).fromNow();
+  return (
+    <div className='message-wrap'>
+        <div className='message-with-user'>
+        <img src={user.avatar}alt="user avatar" className='user-avatar-on-msg'/>
+        <p className='message-conetent'>
+            <p>{timeFromNow(timestamp)}</p>
+            {conetent}
+            </p>
+            </div>
+    </div>
+  )
+} 
+export default Message;
--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -1,23 +1,60 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import MessageHeader from './MessageHeader';
 import "./Messages.css";
 import firebase from '../../firebase';
 import SendMessage from './SendMessage'
+import { useSelector } from 'react-redux/es/hooks/useSelector';
+import Message from './Message';
 
-export default function Messages() {
+const Messages = () => {
+  const {currentChannelID}= useSelector((state)=>  state.channelReducer);
   const [messageData, setMessageData] =  useState({
     messagesRef: firebase.database().ref("messages"),
+    channel: '',
+    channelMessages:'',
   });
+  useEffect(()=>{
+    if(currentChannelID) {
+      setMessageData((e) => ({...e,
+        ["channel"]: currentChannelID,
+    }));  
+       addListener(currentChannelID)
+    }
+},[currentChannelID])
 
-  // const {messagesRef} = message;
-  console.log("REF at MESSAGE to be passed: ",messageData.messagesRef)
+  const addListener = (channelId) => {
+   displayMessageListner(channelId)
+  }
+
+  const displayMessageListner = (channelID) =>{ 
+let loadedMessages = [];
+messageData.messagesRef.child(channelID).on('child_added', collect => {
+  loadedMessages.push(collect.val());
+  setMessageData((e) => ({
+    ...e,
+    ["channelMessages"]: loadedMessages,
+  }));
+} )
+
+  }
+  const {channelMessages} = messageData;
+  try {
   return (
     <section className='Message-section'>
       <MessageHeader/>
       <div className='message-body'>
-        
+        {channelMessages.map((m) => (
+            <Message key={m.user.id} message={m} />
+        ))
+}
       </div>
       <SendMessage messageprop={messageData}/>
     </section>
   )
 }
+catch(err) {
+  console.log(err);
+}
+return "";
+}
+export default Messages; 
