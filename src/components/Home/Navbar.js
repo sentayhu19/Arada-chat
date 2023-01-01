b834fc11fe54b6594f8da9e53cf885fd5dc79461
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2023-01-01 20:38:20 +0300

    Nav bar mobile menu created

--- src/components/Home/Navbar.js
+++ src/components/Home/Navbar.js
@@ -1,19 +1,54 @@
 import React from 'react'
+import { useState } from 'react';
 import { NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
 import './Navbar.css';
 const Navbar = () => {
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
   return (
     <nav>
         <div className='nav-btns'>
         <NavLink to="/">
           <h1 className='logo-aa' id="navlogo">አrada</h1>
           </NavLink>
-        <ul className='login-signup'>
+          <div className='login-signup'>
             <button className='login-on-home' id='login-d'><NavLink to="/login"> Sign in</NavLink></button>
             <button className='signup-on-home-t' id='login-d' ><NavLink to="/signup" id='signup'>Sign up</NavLink> </button>
-        </ul>
         </div>
-
+         
+        {click  ? 
+        <div className='mobile-nav-wraper'>
+          <div className='mobile-nav'>
+            <div className='nav-flex'>
+            <h1 className='nav-logo'>አrada</h1>
+          <div onClick={handleClick}>
+           <FontAwesomeIcon icon={faXmark} />
+           </div>
+           </div>
+           <div className='mob-nav-btn'>
+        <button className='login-on-home' id='login-nav'>
+          <NavLink to="/login"> Sign in</NavLink>
+          </button>
+        <button className='signup-on-home-t' id='login-nav' ><NavLink to="/signup" id='signup'>
+          Sign up</NavLink> 
+          </button>
+          </div>
+        </div>
+  </div>
+        
+         :
+         <div onClick={handleClick} >
+         <FontAwesomeIcon icon={faBars} />
+         </div>
+          }
+       
+        
+       
+        </div>
+      
     </nav>
   )
 }
